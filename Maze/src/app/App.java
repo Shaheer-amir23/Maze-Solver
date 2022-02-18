de496b4a0add637e307f51284a337f4a710aaa84
Shaheer-amir23 <95835492+Shaheer-amir23@users.noreply.github.com>
2022-02-17 19:31:11 -0500

    Add files via upload
    Walls are represented with a "%". The path is represented with a "+". The open spaces are represented with a ".". The entrance is "e" and the exit is "x".

---
+++ Maze/src/app/App.java
@@ -0,0 +1,73 @@
+package app;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner input = new Scanner(new File("Maze.txt"));
+        String temp = input.nextLine();
+        String[] temporary = temp.split(",");
+        int row = Integer.parseInt(temporary[0]), startRow = 0;
+        int column = Integer.parseInt(temporary[1]), startColumn = 0;
+        char[][]floor = new char[row][column];
+
+        while(input.hasNextLine()){
+            for (int i = 0; i < floor.length; i++){
+                floor[i] = input.nextLine().toCharArray();
+            }
+        }
+        for (int i = 0; i < floor.length; i++) {
+            for (int j = 0; j < floor[0].length; j++) {
+                if(floor[i][j] == 'e'){
+                    startRow = i;
+                    startColumn = j;
+                }
+            }
+        }
+        display(startRow, startColumn, floor);
+        findPath( startRow, startColumn, floor);
+        display(startRow, startColumn, floor);
+        input.close();
+    }
+    public static void display(int Srow, int Scolumn, char[][] floor) {
+        floor[Srow][Scolumn] = 'e';
+        for (int i = 0; i < floor.length; i++) {
+            for (int j = 0; j < floor[0].length; j++) {
+                System.out.print(floor[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static boolean findPath(int row, int column, char[][]floor) {
+        if (row < 0 ||  row >= floor.length || column < 0 || column >= floor[0].length) {
+            return false;
+        }
+        if (floor[row][column] == 'x') {
+            return true;
+        }
+        if (floor[row][column] == '%' || floor[row][column] == '+') {
+            return false;
+        }
+        floor[row][column] = '+';
+
+        if (findPath( row - 1, column, floor)) {
+            return true;
+        }
+        if (findPath( row + 1,column, floor)) {
+            return true;
+        }
+        if (findPath(row, column - 1,floor)) {
+            return true;
+        }
+        if (findPath(row, column + 1, floor)) {
+            return true;
+        }
+        floor[row][column] = '.';
+
+        return false;
+    }
+}
